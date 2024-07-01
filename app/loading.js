0539b50bab4d517c41d5ea5c286a5fbd3b606109
Marina Capan <marina.capanzg@gmail.com>
2024-07-01 23:08:08 +0200

    loading page

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+
+import classes from './loading.module.css';
+
+const PostsLoadingPage = () => {
+  return <p className={classes.loading}>Fetching posts...</p>
+}
+
+export default PostsLoadingPage;
